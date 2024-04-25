a2f136587caed560e9b470174abb942f66ec9ccd
rizwan-muhd <rizwankk448@gmail.com>
2024-04-25 12:20:46 +0530

    page protected

--- src/views/HomePage.js
+++ src/views/HomePage.js
@@ -1,8 +1,15 @@
 import React, { useEffect, useState } from "react";
 import RowPost from "../components/RowPost/Rows";
 import axios from "axios";
+import LoginPage from "./LoginPage";
 
 function HomePage() {
+  const token = localStorage.getItem("accessToken");
+  if (!token) {
+    console.log("Token not found in local storage");
+    // window.location = "/login";
+    return <LoginPage />;
+  }
   return (
     <div>
       <RowPost />
